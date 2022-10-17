a2ad13d65243e265555c8abc20b47f8822848a99
Erik <71232753+erischl@users.noreply.github.com>
2022-10-17 07:42:33 -0500

    Add files via upload

---
+++ Order.py
@@ -0,0 +1,10 @@
+parts_received = (0);
+full_parts_list = (0,0);
+full_parts_list_by_name = ('');
+
+
+setOrderStatus{
+        
+    parts_received = input("Enter the part received: ");
+    full_parts_list = full_parts_list + parts_received;
+}
---
+++ Part.py
@@ -0,0 +1,6 @@
+part_number = 0;
+part_name = '';
+
+editPartName(part_number){
+    part_name = input("Enter the new part name: ");
+}
