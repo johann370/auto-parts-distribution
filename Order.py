3b3a64b88017de67aa29692c0390ff1bb59928a9
Erik <71232753+erischl@users.noreply.github.com>
2022-10-23 16:39:13 -0500

    Update Order.py

--- Order.py
+++ Order.py
@@ -3,7 +3,7 @@ full_parts_list = (0,0)
 full_parts_list_by_name = ('')
 
 
-def setOrderStatus()
+def setOrderStatus():
         
     parts_received = input("Enter the part received: ")
     full_parts_list = full_parts_list + parts_received
