0dd2783754ad0f890fc6af0aad4919c691530531
Erik <71232753+erischl@users.noreply.github.com>
2022-10-23 16:38:30 -0500

    Update Payment.py

--- Payment.py
+++ Payment.py
@@ -2,7 +2,6 @@ part_payment_amount = 0.00
 full_payment_amount = 0.00
 payment_process_completed = 0
 
-inputPartPaymentAmount{
+def inputPartPaymentAmount():
     payment_amount = input("Enter the payment amount for the part: ")
     full_payment_amount += part_payment_amount
-}
