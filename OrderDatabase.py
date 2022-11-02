8fce952a2f6b8aee877abd2c2ac6244f17dfa64f
Erik <71232753+erischl@users.noreply.github.com>
2022-11-02 13:58:58 -0500

    Update OrderDatabase.py

--- OrderDatabase.py
+++ OrderDatabase.py
@@ -1,1 +1,18 @@
+import sqlite3
 
+
+CREATE TABLE IF NOT EXISTS orders(
+  id integer PRIMARY KEY,
+  parts list,
+  account_number integer
+)
+  
+CREATE TABLE IF NOT EXISTS employees(
+  employee_id integer PRIMARY KEY,
+  
+)
+  
+
+CREATE TABLE IF NOT EXISTS customers(
+ customer_id integer PRIMARY KEY 
+)
