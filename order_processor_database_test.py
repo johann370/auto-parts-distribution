8333b504bc67539f8883b27b2456fdd2a889e283
Erik <71232753+erischl@users.noreply.github.com>
2022-11-18 21:17:30 -0600

    Update order_processor_database_test.py

--- order_processor_database_test.py
+++ order_processor_database_test.py
@@ -9,11 +9,9 @@ class TestDatabaseMethods(unittest.TestCase):
         self.conn = self.database.connection
         self.cursor = self.conn.cursor()
 
-        self.cursor.execute(
-            'CREATE TABLE order(id INTEGER PRIMARY KEY, parts_received, full_parts_list, full_parts_list_by_name)')
-        info = [('test name', (1), (1,2), 'part1, part2'),('test 2', (2), (2,3), 'part2, part3'),('test 3', (3), (4,5,6), 'part4, part5, part6'),('test4', (3), (7,8,9), 'part7, part8, part9'),('test5', (5), (7,8,9,10,11), 'part7, part8, part9, part10, part11')]
-        self.cursor.executemany(
-            'INSERT INTO order(id, parts_received, full_parts_list, full_parts_list_by_name) VALUES (?, ?, ?, ?, )', info)
+        self.cursor.execute('CREATE TABLE orders(order_id INTEGER PRIMARY KEY, parts_received, full_parts_list, full_parts_list_by_name)')
+        info = [['1', '1,2', 'part1, part2'],['2', '2,3', 'part2, part3'],['3','4,5,6', 'part4, part5, part6'],['3', '7,8,9', 'part7, part8, part9'],['5', '7,8,9,10,11', 'part7, part8, part9, part10, part11']]
+        self.cursor.executemany('INSERT INTO orders(order_id, parts_received, full_parts_list, full_parts_list_by_name) VALUES (?,?,?,?)', info)
 
         self.conn.commit()
 
@@ -21,11 +19,11 @@ class TestDatabaseMethods(unittest.TestCase):
         self.conn.close()
 
     def test_get_order(self, order_id):
-        order = Order(order_id= 1, parts_received = (2,3), full_parts_list = (2,3,5), full_parts_list_by_name = 'part2, part3, part5')
+        orders = Order(order_id= 1, parts_received = '2,3', full_parts_list = '2,3,5', full_parts_list_by_name = 'part2, part3, part5')
 
         database_order = self.database.get_order(4)
 
-        self.assertEqual(database_order, order)
+        self.assertEqual(database_order, orders)
 
 
         
