074842bb8cbc82c9f84e1d4199c6192ccf1930f9
Erik <71232753+erischl@users.noreply.github.com>
2022-11-23 13:01:13 -0600

    Update Order.py

--- Order.py
+++ Order.py
@@ -5,9 +5,9 @@ from dataclasses import dataclass
 class Order:
     
     id = 0
-    parts_received = (0)
-    full_parts_list = (0,0)
-    full_parts_list_by_name = ('')
+    parts_received = '0'
+    full_parts_list = '0,0'
+    full_parts_list_by_name = ''
 
 
    
